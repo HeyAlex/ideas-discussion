7538a9a9dd381718f4253effd0be0df1bc2974ab
a.fialko <a.fialko@corp.mail.ru>
2019-01-03 21:51:05 +0300

    initial commit

---
+++ src/main/kotlin/com/github/heyalex/ideasdiscussion/IdeasDiscussionApplication.kt
@@ -0,0 +1,12 @@
+package com.github.heyalex.ideasdiscussion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdeasDiscussionApplication
+
+fun main(args: Array<String>) {
+	runApplication<IdeasDiscussionApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/github/heyalex/ideasdiscussion/IdeasDiscussionApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.github.heyalex.ideasdiscussion
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class IdeasDiscussionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
