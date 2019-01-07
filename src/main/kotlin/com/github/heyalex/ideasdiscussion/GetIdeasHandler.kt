0ca96931cb589e1714e71a6f2199cf46523e5ed4
a.fialko <a.fialko@corp.mail.ru>
2019-01-07 21:45:54 +0300

    add beans initializing and handler idea route

---
+++ src/main/kotlin/com/github/heyalex/ideasdiscussion/GetIdeasHandler.kt
@@ -0,0 +1,15 @@
+package com.github.heyalex.ideasdiscussion
+
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+
+class GetIdeasHandler {
+
+    fun handle(request: ServerRequest) : Mono<ServerResponse> {
+        println(request.toString())
+        return ServerResponse.ok().body(fromObject(arrayOf(1, 2, 3))).toMono()
+    }
+}
--- src/main/kotlin/com/github/heyalex/ideasdiscussion/IdeasDiscussionApplication.kt
+++ src/main/kotlin/com/github/heyalex/ideasdiscussion/IdeasDiscussionApplication.kt
@@ -1,11 +1,29 @@
 package com.github.heyalex.ideasdiscussion
 
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.beans
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
+import org.springframework.web.reactive.function.server.HandlerStrategies
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder
 import reactor.netty.http.server.HttpServer
 import java.time.Duration
 
-class IdeasDiscussionApplication
-
 fun main(args: Array<String>) {
+    val context = GenericApplicationContext()
+
+    beans {
+        bean<Router>()
+        bean<GetIdeasHandler>()
+        bean(WebHttpHandlerBuilder.WEB_HANDLER_BEAN_NAME) {
+            RouterFunctions.toWebHandler(
+                ref<Router>().generateRouter(),
+                HandlerStrategies.builder().build()
+            )
+        }
+    }.initialize(context)
+
+    context.refresh()
 
     HttpServer.create()
         .host("0.0.0.0")
